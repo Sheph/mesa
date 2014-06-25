f13f7936c63371415a41321f6753bd141ba084f6
Stanislav Vorobiov <s.vorobiov@samsung.com>
2014-06-25 11:29:02 +0400

    only build code that's needed for graw to work
    llvmpipe only for now

--- build_llvm.sh
+++ build_llvm.sh
@@ -1,8 +1,6 @@
 rm -rf ./build_llvm
 mkdir -p ./build_llvm
 cd ./build_llvm
-../configure --with-gallium-drivers=swrast --with-dri-drivers=swrast --disable-dri3 \
---enable-gallium-llvm --enable-shared-glapi=no --enable-gallium-tests --prefix=$PWD
+../configure --with-gallium-drivers=swrast --with-dri-drivers= --disable-dri3 \
+--disable-opengl --disable-egl --disable-dri --enable-gallium-llvm --enable-shared-glapi=no --enable-gallium-tests --prefix=$PWD
 make -j4
-mkdir -p $PWD/lib/dri
-ln -s $PWD/lib/gallium/swrast_dri.so $PWD/lib/dri/swrast_dri.so
